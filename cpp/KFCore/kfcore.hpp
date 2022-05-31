1df3423c41ce6564381548e45faf73dd3ca812da
baixiaosheng520 <94878747+baixiaosheng520@users.noreply.github.com>
2022-05-31 20:22:46 +0800

    change paras of update and predict

--- cpp/KFCore/kfcore.hpp
+++ cpp/KFCore/kfcore.hpp
@@ -16,7 +16,7 @@ namespace Kalman {
 	*/
 	template <int dimz, int dimx>
 	struct KFCore {
-		Matf<dimx, 1> x;     // 上一次卡尔曼滤波后验估计
+		Matf<dimx, 1> xp;     // 上一次卡尔曼滤波后验估计
 		Matf<dimx, dimz> K;  // 卡尔曼增益
 		Matf<dimz, dimz> R;  // 预测过程噪声偏差的协方差
 		Matf<dimx, dimx> Q;  // 测量噪声偏差
@@ -24,17 +24,19 @@ namespace Kalman {
 		Matf<dimx, dimx> A;  // 状态转移矩阵
 		Matf<dimz, dimx> H;  // 观测矩阵
 
-		Matf<dimx, 1> predict() {
-			Matf<dimx, 1> xp = A * x;  // 计算先验估计
+		Matf<dimx, 1> predict(Ref<Matf<dimx, 1>> x) {
+			xp = A * x;  // 计算先验估计
 			P = A * P * A.transpose() + Q;  // 先验协方差
 			return xp;
 		}
 
-		Matf<dimx, 1> update(Ref<Matf<dimz, 1>> z, Ref<Matf<dimx, 1>> xp) {
+		Matf<dimx, 1> update(Ref<Matf<dimz, 1>> z) {
 			K = P * H.transpose() * (H * P * H.transpose() + R).inverse();  // 卡尔曼增益
-			x = xp + K * (z - H * xp);  // 后验估计
+			Matf<dimx, 1> x = xp + K * (z - H * xp);  // 后验估计
 			P = (Matf<dimx, dimx>::Identity() - K * H) * P;  // 协方差矩阵
 			return x;
 		}
+		
+		
 	};
 }
